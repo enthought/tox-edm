0ad339394956e0a3e4e51b2d7dc98be2170c3104
Ioannis Tziakos <mail@itziakos.gr>
2018-07-14 01:37:13 +0100

    Rework code to use the action.popen

--- tox_edm/__init__.py
+++ tox_edm/__init__.py
@@ -87,8 +87,8 @@ def tox_runtest(venv, redirect):
         venv.status = 0
         session.make_emptydir(envconfig.envtmpdir)
         envconfig.envtmpdir.ensure(dir=1)
-        cwd = envconfig.changedir
         env = venv._getenv(testcommand=True)
+        cwd = envconfig.changedir
         # Display PYTHONHASHSEED to assist with reproducibility.
         action.setactivity(
             "runtests", "PYTHONHASHSEED={!r}".format(
@@ -97,7 +97,35 @@ def tox_runtest(venv, redirect):
             message = "commands[%s] | %s" % (
                 i, ' '.join([str(x) for x in argv]))
             action.setactivity("runtests", message)
-            edm('run', '-e', action.venvname, '--', *argv, env=env)
+            # check to see if we need to ignore the return code
+            # if so, we need to alter the command line arguments
+            ignore_return = argv[0].startswith("-")
+            if ignore_return:
+                if argv[0] == "-":
+                    del argv[0]
+                else:
+                    argv[0] = argv[0].lstrip("-")
+            edm = venv._venv_lookup('edm')
+            argv = [edm, 'run', '-e', action.venvname, '--'] + argv
+            try:
+                action.popen(
+                    argv, cwd=cwd, env=env, redirect=redirect,
+                    ignore_ret=ignore_return)
+            except exception.InvocationError as err:
+                if envconfig.ignore_outcome:
+                    msg = "command failed but result from testenv is ignored\ncmd:"
+                    session.report.warning("{} {}".format(msg, err))
+                    venv.status = "ignored failed command"
+                    continue  # keep processing commands
+
+                session.report.error(str(err))
+                venv.status = "commands failed"
+                if not envconfig.ignore_errors:
+                    break  # Don't process remaining commands
+            except KeyboardInterrupt:
+                venv.status = "keyboardinterrupt"
+                session.report.error(venv.status)
+                raise
     return True
 
 
